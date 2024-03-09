f289e2d442225e018db6da092fe7044a7f5b2585
PhHien <161614082+PhHien@users.noreply.github.com>
2024-03-09 20:40:01 +0700

    Add files via upload

--- Cat.java
+++ Cat.java
@@ -3,11 +3,27 @@ package Inheritance;
 public class Cat extends Animal{
 	private String type;
 	
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getType() {
+		return this.type;
+	}
+
+	/*
+	 * public String toString() { return name + " " + age + " " + this.type; }
+	 */
+	
+	@Override
+	public String toString() {
+		return "Cat [type=" + type + "]";
+	}
 	public static void main(String[] args) {
 		Cat cat = new Cat();
 		cat.setAge(3);
 		cat.setName("aaaa");
 		System.out.println("Ten: " + cat.getName());
 		System.out.println("Tuoi: " + cat.getAge());
+		System.out.println(cat.toString());
 	}
 }
