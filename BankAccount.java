f6b0c871109360aaa53e4c99effb259ab110ab37
PhHien <161614082+PhHien@users.noreply.github.com>
2024-03-10 10:09:56 +0700

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -2,5 +2,28 @@ package Inheritance;
 
 public class BankAccount {
 	private String accountNumber ;
-	boolean balance;
+	private boolean balance;
+	
+	 public BankAccount(String accountNumber, boolean balance) {
+		 this.accountNumber = accountNumber; 
+		 this.balance = balance; 
+	 }
+	 
+	public String getAccountNumber() {
+		return this.accountNumber;
+	}
+	public boolean getBalance() {
+		return this.balance;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public void setBalance(boolean balance) {
+		this.balance = balance;
+	}
+	@Override
+	public String toString() {
+		return "BankAccount [accountNumber=" + accountNumber + ", balance=" + balance + "]";
+	}
+	
 }
