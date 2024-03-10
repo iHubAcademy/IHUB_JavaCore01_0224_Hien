9a0c1556bda6174f9e63b8a7a5fbfda468494b2d
PhHien <161614082+PhHien@users.noreply.github.com>
2024-03-10 10:10:32 +0700

    Add files via upload

--- BasicSavings.java
+++ BasicSavings.java
@@ -2,4 +2,29 @@ package Inheritance;
 
 public class BasicSavings extends BankAccount {
 	private float interestRate;
+	
+	public BasicSavings(String accountNumber, boolean balance, float interestRate) {
+		super(accountNumber, balance);
+		this.interestRate = interestRate;
+	}
+	public float getInterestRate() {
+		return this.interestRate;
+	}
+	public void setInterestRate(float interestRate) {
+		this.interestRate = interestRate;
+	}
+	@Override
+	public String toString() {
+		return super.toString() + " BasicSavings [interestRate=" + interestRate + "]";
+	}
+	
+	public static void main(String[] args) {
+		String accountNumber = "01123ABCD";
+		boolean balance = false;
+		float interestRate = 3.2F;
+		BasicSavings newBasicSavings = new BasicSavings(accountNumber, balance, interestRate);
+		newBasicSavings.setBalance(true);
+		newBasicSavings.setInterestRate(3.3F);
+		System.out.println(newBasicSavings.toString());
+	}
 }
